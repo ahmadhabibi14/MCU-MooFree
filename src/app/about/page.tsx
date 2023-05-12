84e4b1864f5e0680f465861903cb25c727ca7397
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-05-13 05:39:37 +0800

    About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+   title: 'About',
+   description: 'Your Marvel movies',
+   openGraph: {
+     title: 'MovFree',
+     images: '/next.svg',
+   },
+ }
+
+export default function About() {
+   return (
+      <div>
+         <h2>About</h2>
+      </div>
+   )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from 'next/image'
 
+export const metadata = {
+  title: 'MCU - MovFree',
+  description: 'Your Marvel movies',
+  openGraph: {
+    title: 'MovFree',
+    images: '/next.svg',
+  },
+}
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
