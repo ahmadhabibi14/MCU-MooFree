392adc9ad4bbfe7d3599c67c4e66621e17e87eda
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-05-18 06:53:55 +0800

    Color Dominant

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,34 +1,11 @@
 import { fetchMovieData } from "@/api/fetch_movie_data";
 import { getMovieById, getMoviePoster, movieObjectMap } from "@/data/movies/movie_page_details";
 import Image from "next/image"
-// import ColorThief from "color-thief-ts/node";
-// import Sharp from "sharp";
-// const { getPaletteFromURL } = require('color-thief-node');
 const ColorThief = require('colorthief');
 
 export function generateStaticParams() {
   return Object.values(movieObjectMap).map((obj) => obj.id);
 }
-
-// const fetchImageBuffer = async (url: string): Promise<Buffer> => {
-//   const response = await fetch(url);
-//   const buffer = await response.arrayBuffer();
-//   return buffer;
-// };
-
-// const getImageDominantColor = async (url: string) => {
-//    const colorThief = new ColorThief();
-//    const image = await fetch(url).then(
-//       (res) => res.arrayBuffer()
-//    ).then(
-//       (arrayBuffer) => Buffer.from(arrayBuffer)
-//    );
-//    console.log(image)
-
-//    const color = colorThief.getColor(image);
-
-//    return color;
-// };
 
 export default async function Post({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -36,17 +13,13 @@ export default async function Post({ params }: { params: { id: string } }) {
   const imgPoster = getMoviePoster(id)
   const movieData = await fetchMovieData(movieId)
 
-  // const dominantColor = await getImageDominantColor(`https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`);
-  // console.log(dominantColor)
-
-  // const colorPallete = await getPaletteFromURL(`https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`);
-  // console.log(colorPallete)
   const img = `https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`
   const colors = await ColorThief.getPalette(img, 5)
-  console.log(colors)
+  const bgBackdrop = colors[3]
+  const rgbaBackdrop = `rgba(${bgBackdrop[0]}, ${bgBackdrop[1]}, ${bgBackdrop[2]}, 0.75)`;
   return (
     <div className="h-fit">
-      <header className="">
+      <header className="text-zinc-200">
          <div className="w-full h-[400px] overflow-hidden">
             <Image
                src={`https://image.tmdb.org/t/p/original/${movieData.backdrop_path}`}
@@ -56,7 +29,8 @@ export default async function Post({ params }: { params: { id: string } }) {
                className="object-cover w-full h-auto"
             />
          </div>
-         <div className="h-[460px] w-full top-0 absolute z-20 bg-gradient-to-t from-white dark:from-zinc-900 to-transparent"></div>
+         <div style={{backgroundColor: rgbaBackdrop}} className="h-[460px] w-full top-0 absolute z-10"></div>
+         <div className="hidden dark:block h-[200px] w-full top-[260px] absolute z-20 bg-gradient-to-t dark:from-zinc-900 to-transparent "></div>
          {/* Properties*/}
          <div className="z-30 absolute top-[64px] w-full h-[438px] px-28 py-16 flex flex-row gap-12">
             <div className="w-44">
